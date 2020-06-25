45296978576f89cb0679700048eebc5825334ed9
awhitbeck <whitbeck.andrew@gmail.com>
2020-06-25 16:53:01 -0500

    initial commit

---
+++ count_hits_threshold_scan.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from ts_digi_container import *
+import ROOT as r
+from array import array
+
+## load and initialize container
+cont = ts_digi_container('../ldmx-sw/single_test_100k.root','LDMX_Events')
+cont.get_digi_collection('trigScintDigisTag_sim')
+cont.get_digi_collection('trigScintDigisUp_sim')
+cont.get_digi_collection('trigScintDigisDn_sim')
+
+accuracy=[]
+threshold=[]
+
+for th in range(0,210,10):
+    threshold.append(th)
+
+    ## get the true number of beam electrons for all events
+    electrons=cont.get_num_beam_electrons()
+
+    ## get hits from tagger array for all events and predict num. electrons
+    tag_hits=cont.count_hits('trigScintDigisTag_sim',th)
+    pred=np.divide(np.add(tag_hits,1),2)
+
+    acc = np.count_nonzero(electrons==pred)/float(cont.tree.numentries)
+    accuracy.append(acc)
+    print "threshold:",th,"accuracy:",acc
+
+gr=r.TGraph(len(threshold),array('f',threshold),array('f',accuracy))
+
+can=r.TCanvas('can','can',500,500)
+gr.Draw()
+can.SaveAs("tresholdScan.png")
