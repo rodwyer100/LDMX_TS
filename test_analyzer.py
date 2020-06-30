ba834fc2a19d2d65c0c118fa2960e1ccc8eb8b99
bryngemark <lene.bryngemark@cern.ch>
2020-06-30 13:10:25 -0700

    Update test_analyzer.py

--- test_analyzer.py
+++ test_analyzer.py
@@ -2,7 +2,6 @@ from ts_digi_container import *
 import ROOT as r
 
 ## initialize container
-#cont = ts_digi_container('../ldmx-sw/single_test_100k.root','LDMX_Events')
 cont = ts_digi_container('test.root','LDMX_Events')
 cont.get_digi_collection('trigScintDigisTag_sim')
 cont.get_digi_collection('trigScintDigisUp_sim')
