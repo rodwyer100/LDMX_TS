6a8a8e8c98353df0870b1893379d591471dc60a2
Lene Kristian Bryngemark <lenebryngemark@PHY-C02YV2HJLVDM.local>
2020-06-30 10:58:39 -0700

    Fix paths to run first plotting out of the box
    The path to the test file and the style file were off. Now set to current dir.
    Also set up saving the resuting canvas to a .png

--- test_analyzer.py
+++ test_analyzer.py
@@ -2,13 +2,14 @@ from ts_digi_container import *
 import ROOT as r
 
 ## initialize container
-cont = ts_digi_container('../ldmx-sw/single_test_100k.root','LDMX_Events')
+#cont = ts_digi_container('../ldmx-sw/single_test_100k.root','LDMX_Events')
+cont = ts_digi_container('test.root','LDMX_Events')
 cont.get_digi_collection('trigScintDigisTag_sim')
 cont.get_digi_collection('trigScintDigisUp_sim')
 cont.get_digi_collection('trigScintDigisDn_sim')
 
 ## configuration for pretty root plots
-r.gROOT.ProcessLine(".L ~/tdrstyle.C")
+r.gROOT.ProcessLine(".L tdrstyle.C")
 r.gROOT.ProcessLine("setTDRStyle()")
 
 ## initialize root histogram
@@ -30,7 +31,10 @@ for i in range(cont.tree.numentries):
         hist.Fill(pe)
 
 #plot!
+c1 = r.TCanvas("c1", "hist canvas", 600,  500)
 hist.SetFillColor(2)
 hist.SetLineColor(4)
 hist.SetLineStyle(2)
 hist.Draw()
+
+c1.SaveAs( hist.GetName()+".png")
