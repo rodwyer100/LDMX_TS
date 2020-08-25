a8dc752f5fcad044719e14d56255bafc1c04a0c2
Lene Kristian Bryngemark <lene.bryngemark@cern.ch>
2020-08-25 11:10:14 -0700

    Undo of input file name change
    Accidentally set it to a file I only have locally

--- test_analyzer.py
+++ test_analyzer.py
@@ -2,7 +2,7 @@ from ts_digi_container import *
 import ROOT as r
 
 ## initialize container
-cont = ts_digi_container('clustered_ldmx_upstreamElectron_run1_1e_10000events_digi.root','LDMX_Events')
+cont = ts_digi_container('test.root','LDMX_Events')
 cont.get_digi_collection('trigScintDigisTag_sim')
 cont.get_digi_collection('trigScintDigisUp_sim')
 cont.get_digi_collection('trigScintDigisDn_sim')
