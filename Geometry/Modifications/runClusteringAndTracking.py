41fd298bf5f97c8bda2e0f425ea1db8abb4bb88e
rodwyer100 <rodwyer100@tamu.edu>
2021-03-25 19:56:02 -0500

    Add files via upload

---
+++ Geometry/Modifications/runClusteringAndTracking.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python                                                                                                                                                                                      
+
+import sys
+import os
+
+
+# we need the ldmx configuration package to construct the object
+
+from LDMX.Framework import ldmxcfg
+
+# first, we define the process, which must have a name which identifies this processing pass ("pass name").
+# it's pretty arbitrary but you will see it in the final collection names.
+
+p=ldmxcfg.Process("digi")
+
+#load the template trigger scintillator clustering and tracking configuration files
+
+from LDMX.TrigScint.trigScint import TrigScintClusterProducer
+from LDMX.TrigScint.trigScint import trigScintTrack
+
+
+#clustering: seeding threshold 
+tagSeed = 30. 
+upSeed= 30.
+downSeed = upSeed
+#cluster width (upper limit on nHits/cluster)
+tagWidth = 2      # can be up to 3 (default)
+#cluster threshold: hits below this PE count are neglected
+tagClThr = 5.  # default: 1. here use something >> typical electronics noise, which is 1 (sometimes 2) PE
+
+#tracking: max distance between cluster candidate position and seed cluster position
+maxDelta = 1.25
+
+clusteringVerbosity=0
+trackingVerbosity=0
+
+#maximum number of events to process  
+nEv = 100
+
+
+if len(sys.argv) < 1 :
+    print("The number of beam electrons has to be specified. Use (positional) argument 1 for it.")
+    exit(1)
+else :
+    nPart=sys.argv[1]   #number of beam electrons to use
+
+    
+runNb = nPart # "run number"; for file name uniqueness when there are multiple runs -- uses multiplicity as default
+
+
+if len(sys.argv) > 2 :  
+    inputName=sys.argv[2]  #specify the input name if default is not desired                                                                                             
+else:
+    inputName="ldmx_upstreamElectron_run%i_%se_%sevents.root" %( runNb, nPart, nEv)
+    print("Using input file name: "+inputName)
+
+    
+if len(sys.argv) > 3 :
+    outputName=sys.argv[3]  #specify the output name if default is not desired
+else:
+    outputName="clustered_"+inputName
+    print("Using output file name: "+outputName)
+
+    
+# ------------------- all set; propagate these settings in detail, and run  ---------------
+
+
+# Define the sequence of event processors to be run
+
+# --- clustering ---
+clTag=TrigScintClusterProducer.tagger()
+clUp=TrigScintClusterProducer.up()
+clDown=TrigScintClusterProducer.down()
+
+clTag.verbosity = clusteringVerbosity
+clUp.verbosity = clusteringVerbosity
+clDown.verbosity = clusteringVerbosity
+
+# here the seeds etc could also be set
+clTag.max_cluster_width = tagWidth
+clTag.clustering_threshold = tagClThr
+clTag.seed_threshold = tagSeed
+clUp.seed_threshold = upSeed
+clDown.seed_threshold = downSeed
+
+# --- tracking ---
+trigScintTrack.verbosity = trackingVerbosity
+trigScintTrack.delta_max = maxDelta
+
+
+p.maxEvents=nEv
+p.sequence = [ clTag, clUp, clDown , trigScintTrack ]
+
+# Provide the list of input files to run on
+#p.inputFiles=["~/raid/LDMX/trigger_pad_sim/test.root"]
+p.inputFiles=[ inputName ] #"ldmx_sim_events.root"]
+
+# Provide the list of output files to produce, either one to contain 
+# the results of all input files or one output file name per input file name
+#p.outputFiles=["ldmx_digi_events.root"]
+p.outputFiles=[outputName]
+
+# Utility function to interpret and print out the configuration to the screen
+print(p)
+
---
+++ Geometry/Modifications/runSimAndDigi.py
@@ -0,0 +1,121 @@
+ #!/usr/bin/python
+
+
+import sys
+import os
+
+# we need the ldmx configuration package to construct the object
+from LDMX.Framework import ldmxcfg
+
+# first, we define the process, which must have a name which identifies this processing pass ("pass name").
+# it's pretty arbitrary but you will see it in the final collection names.
+p=ldmxcfg.Process("sim")
+
+
+from LDMX.SimCore import generators as gen
+from LDMX.SimCore import simulator
+from LDMX.Biasing import filters
+from LDMX.SimCore import simcfg
+
+from LDMX.TrigScint.trigScint import TrigScintDigiProducer
+
+import LDMX.Ecal.EcalGeometry
+
+nEv = 10000
+
+if len(sys.argv) < 1 :
+    print("The number of beam electrons has to be specified. Use (positional) argument 1 for it.")
+    exit(1)
+else :
+    nPart=sys.argv[1]   #number of beam electrons to use 
+
+    
+if len(sys.argv) > 2 :  # "run number"; mostly for guaranteeing uniqueness -- uses multiplicity as default
+    runNb=int(sys.argv[2])
+else:
+    runNb=int(nPart)
+
+if len(sys.argv) > 3 :
+    outputName=sys.argv[3]  #specify the output name if default is not desired 
+else:
+    outputName="ldmx_upstreamElectron"
+
+doPoisson = False #set to true to get poisson varied number of beam electrons instead of fixed
+
+
+# ------------------- all set; setup in detail, and run with these settings ---------------
+
+
+
+mySim = simulator.simulator("mySim")
+mySim.runNumber = runNb
+mySim.description = "Upstream 4 GeV "+nPart+"-electron beam"
+mySim.beamSpotSmear = [20., 80., 0] #mm
+
+# get the path to the installed detector description
+
+from LDMX.Detectors.makePath import *
+                                                                      
+mySim.setDetector( 'ldmx-det-v12', True  ) #the true should tell it to include scoring planes
+mySim.scoringPlanes = makeScoringPlanesPath('ldmx-det-v12')
+
+
+mpgGen = gen.multi( "mgpGen" ) # this is the line that actually creates the generator
+mpgGen.vertex = [ -27.926, 0., -700 ] # mm
+mpgGen.nParticles = int(nPart)
+mpgGen.pdgID = 11
+mpgGen.enablePoisson = doPoisson 
+
+import math
+theta = math.radians(4.5)
+mpgGen.momentum = [ 4000.*math.sin(theta) , 0, 4000.*math.cos(theta) ] # MeV
+#mpgGen.momentum = [ 380., 0., 3981.909 ] #MeV
+
+
+
+
+#set this as the simulator's generator
+mySim.generators = [ mpgGen ]
+
+#add simulation to the sequence of stuff the code does 
+p.sequence = [ mySim ]
+
+# set the maximum number of events to process
+
+p.maxEvents=nEv
+
+
+tsDigisUp  =TrigScintDigiProducer.up()
+tsDigisTag  =TrigScintDigiProducer.tagger()
+tsDigisDown  =TrigScintDigiProducer.down()
+tsDigisUp.pe_per_mip = 100.
+tsDigisTag.pe_per_mip = tsDigisUp.pe_per_mip
+tsDigisDown.pe_per_mip = tsDigisUp.pe_per_mip
+
+# add these to the sequence of processes the code should run
+p.sequence=[ mySim, tsDigisUp, tsDigisTag, tsDigisDown ]
+
+#drop scoring plane hits we won't be using anyway
+p.keep = [ "drop MagnetScoringPlaneHits", "drop TrackerScoringPlaneHits", "drop HcalScoringPlaneHits"]
+
+
+# Provide the list of output files to produce
+#   When using the simulator to produce events, only one output file is necessary
+
+p.outputFiles=["%s.root" %( outputName)]
+
+#p.outputFiles=["%s_run%i_%se_%sevents.root" %( outputName, runNb, nPart, nEv) ]
+
+
+#some logging stuff, helpful to follow what's going on
+p.termLogLevel = 1 #include info messages
+#print this many events to screen (independent on number of events, except round-off effects when not divisible. so can go up by a factor 2 or so)
+logEvents=20
+if p.maxEvents < logEvents :
+     logEvents = p.maxEvents
+p.logFrequency = int( p.maxEvents/logEvents )
+
+
+# Utility function to interpret and print out the configuration to the screen
+print(p)
+
